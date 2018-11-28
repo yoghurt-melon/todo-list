845e96ba8dcc8172c51dfa10cd69e608bd579f77
yoghurt_melon <kimsb1387@gmail.com>
2018-11-28 15:57:41 +0900

    Project config settings

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                To do List
+            </div>
+        );
+    }
+}
+
+export default App;
